70cff681755cc127189b5f45335f9eadd747cdc4
Satoshi Hatakeyama <satoshi.office@gmail.com>
2014-10-14 18:10:56 +0900

    Initial Commit

--- js/nectar.js
+++ js/nectar.js
@@ -52,28 +52,4 @@ $(function(){
         container: 'body'
     });
 
-    // Scrollspy
-    var $window = $(window);
-    var $body   = $(document.body);
-
-    $body.scrollspy({
-        target: '.sidebar-scrollspy'
-    });
-    $window.on('load', function () {
-        $body.scrollspy('refresh');
-    });
-
-    // Affix 
-    var affixWidth = $('.sidebar-affix').width();
-
-    $('.sidebar-affix').affix({
-        offset: {
-            top: $('#header').outerHeight(),
-            bottom: $('#footer').outerHeight()
-        }
-    }).on('affix.bs.affix', function() {
-        affixWidth = $('.sidebar-affix').width();
-    }).on('affixed.bs.affix', function() {
-        $('.sidebar-affix').width(affixWidth);
-    });
 });
