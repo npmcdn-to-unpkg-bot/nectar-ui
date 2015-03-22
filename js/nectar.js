162c6570c1aaaeeaaf12c581870f71c603d04428
Satoshi Hatakeyama <satoshi.office@gmail.com>
2015-03-23 01:51:49 +0900

    Initial Commit

--- js/nectar.js
+++ js/nectar.js
@@ -10,6 +10,7 @@
 // @codekit-prepend "../assets/jquery.heightLine.js";
 // @codekit-prepend "../assets/jquery.biggerlink.min.js"; 
 // @codekit-prepend "../assets/jquery.smooth-scroll.js"; 
+// @codekit-prepend "../assets/fresco/js/fresco/fresco.js"; 
 // @codekit-prepend "../assets/flickity/dist/flickity.pkgd.min.js"; 
 // @codekit-prepend "../assets/iscroll.js"; 
 // @codekit-prepend "../assets/drawer/js/jquery.drawer.min.js"; 
